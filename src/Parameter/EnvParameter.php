844741ce2e84d2379ab11955c1981364fdc08294
Max Zheltikov <maxcruer@gmail.com>
2024-05-21 10:57:04 +0300

    v1.0.3: Added support for post-processing in EnvParameter.

--- src/Parameter/EnvParameter.php
+++ src/Parameter/EnvParameter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Zheltikov\SimpleContainer\Parameter;
 
+use Closure;
 use Psr\Container\ContainerInterface;
 use Zheltikov\SimpleContainer\ParameterInterface;
 
@@ -12,27 +13,37 @@ class EnvParameter implements ParameterInterface
     public function __construct(
         protected string $name,
         protected mixed $default = null,
+        protected ?Closure $processor = null,
     ) {
     }
 
     public function get(ContainerInterface $container): mixed
     {
         if (array_key_exists($this->name, $_SERVER)) {
-            return $this->processValue($_SERVER[$this->name]);
+            return $this->processValue($this->parseValue($_SERVER[$this->name]));
         }
 
         if (array_key_exists($this->name, $_ENV)) {
-            return $this->processValue($_ENV[$this->name]);
+            return $this->processValue($this->parseValue($_ENV[$this->name]));
         }
 
         if (($value = getenv($this->name)) !== false) {
-            return $this->processValue($value);
+            return $this->processValue($this->parseValue($value));
         }
 
-        return $this->default;
+        return $this->processValue($this->default);
     }
 
     protected function processValue(mixed $value): mixed
+    {
+        if ($this->processor !== null) {
+            $value = ($this->processor)($value);
+        }
+
+        return $value;
+    }
+
+    protected function parseValue(mixed $value): mixed
     {
         if (!is_string($value)) {
             return $value;
